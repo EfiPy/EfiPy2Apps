b604c6e7b7d8c0ffbc6e506886f061c74f4e087a
MaxWu <EfiPy.Core@gmail.com>
2026-06-25 20:56:08 +0800

    [App] sample code for X86 reset method

--- EfiPy2Sample/EfiPy2Reset.py
+++ EfiPy2Sample/EfiPy2Reset.py
@@ -2,63 +2,71 @@
 #
 #   part of EfiPy2
 #
-# Copyright (C) 2025 MaxWu efipy.core@gmail.com
+# Copyright (C) 2025 - 2026 MaxWu efipy.core@gmail.com
 #   GPL-2.0
 #
 
-from EfiPy2.Lib.Reset import Reset
+from EfiPy2.Lib.Reset import Reset, EfiResetCold, EfiResetWarm, EfiResetShutdown, EfiResetPlatformSpecific
 
 def main ():
     import argparse
 
     parser = argparse.ArgumentParser (prog = 'EfiPy2Reset.py')
 
-    parser.add_argument ('-s', '--soft', action = 'store_true', help = 'soft reset via CF9 set as 0x04.')
-    parser.add_argument ('-w', '--warm', action = 'store_true', help = 'warn reset via CF9 set as 0x06.')
-    parser.add_argument ('-c', '--cold', action = 'store_true', help = 'cold reset via CF9 set as 0x0E.')
-    parser.add_argument ('-a', '--acpi', action = 'store_true', help = 'system reset via ACPI.')
-    parser.add_argument ('-k', '--keyboard', action = 'store_true', help = 'system reset via keyboard IO port 60/64.')
-    parser.add_argument ('-p', '--port92', action = 'store_true', help = 'system reset via port 92h.')
-    parser.add_argument ('-u', '--uefi', action = 'store_true', help = 'UEFI gRT.ResetSystem operation')
-    parser.add_argument ('-d', '--shutdown', action = 'store_true', help = 'Shutdown via gRT.ResetSystem')
+    SoftBootArg     = parser.add_argument ('-s', '--soft', action = 'store_true', help = 'soft reset.')
+    WarmBootArg     = parser.add_argument ('-w', '--warm', action = 'store_true', help = 'warn reset.')
+    ColdBootArg     = parser.add_argument ('-c', '--cold', action = 'store_true', help = 'cold reset.')
+    AcpiBootArg     = parser.add_argument ('-a', '--acpi', action = 'store_true', help = 'system reset via ACPI.')
+    KeyboardBootArg = parser.add_argument ('-k', '--keyboard', action = 'store_true', help = 'system reset via keyboard IO port 60/64.')
+    Port92BootArg   = parser.add_argument ('-p', '--port92', action = 'store_true', help = 'system reset via port 92h.')
+    UefiBootArg     = parser.add_argument ('-u', '--uefi', action = 'store_true', help = 'UEFI gRT.ResetSystem operation')
+    ShutdownArg     = parser.add_argument ('-d', '--shutdown', action = 'store_true', help = 'Shutdown via gRT.ResetSystem')
+    DryRunArg       = parser.add_argument ('-r', '--dryrun', action = 'store_true', help = 'Dry run shutdown command')
     args = parser.parse_args ()
 
-    if sum ((args.soft, args.warm, args.cold, args.acpi, args.keyboard, args.port92)) > 1:
-        print ('Only one or none argument are allowed in this program')
+    ResetCommandList = (SoftBootArg, WarmBootArg, ColdBootArg, AcpiBootArg, KeyboardBootArg, Port92BootArg, ShutdownArg)
+
+    if sum ((args.soft, args.warm, args.cold, args.acpi, args.keyboard, args.port92, args.shutdown)) > 1:
+        print (f'Only one or none of below argument are allowed in this program...')
+        for ResetCommand in ResetCommandList:
+            print (ResetCommand.option_strings, ResetCommand.help)
         return
 
-    if args.uefi:
-        import EfiPy2.MdePkg.Uefi.UefiMultiPhase as UefiMultiPhase
+    DryRun = True if args.dryrun else False
 
     if args.soft:
-        Reset.Cf9Reset (0x04)
+        print (f'Process "{SoftBootArg.help}"')
+        Reset.Cf9Reset (0x04, DryRun)
     elif args.warm:
+        print (f'Process "{WarmBootArg.help}"')
         if args.uefi == True:
-            from EfiPy2.MdePkg.Uefi.UefiMultiPhase import EfiResetWarm
-            Reset.UefiOp (EfiResetWarm)
+            Reset.UefiOp (EfiResetWarm, DryRun)
         else:
-            Reset.Cf9Reset (0x06)
+            Reset.Cf9Reset (0x06, DryRun)
     elif args.cold:
+        print (f'Process "{ColdBootArg.help}"')
         if args.uefi == True:
-            from EfiPy2.MdePkg.Uefi.UefiMultiPhase import EfiResetCold
-            Reset.UefiOp (EfiResetCold)
+            Reset.UefiOp (EfiResetCold, DryRun)
         else:
-            Reset.Cf9Reset (0x0E)
+            Reset.Cf9Reset (0x0E, DryRun)
     elif args.acpi:
-        Reset.AcpiReset ()
+        print (f'Process "{AcpiBootArg.help}"')
+        Reset.AcpiReset (DryRun)
     elif args.port92:
-        Reset.PortReset (0x92, 0x01)
+        print (f'Process "{Port92BootArg.help}"')
+        Reset.PortReset (0x92, 0x01, DryRun)
     elif args.keyboard:
-        Reset.KeyboardReset ()
+        print (f'Process "{KeyboardBootArg.help}"')
+        Reset.KeyboardReset (DryRun)
     elif args.shutdown:
-        from EfiPy2.MdePkg.Uefi.UefiMultiPhase import EfiResetShutdown
-        Reset.UefiOp (EfiResetShutdown)
+        print (f'Process "{ShutdownArg.help}"')
+        Reset.UefiOp (EfiResetShutdown, DryRun)
     elif args.uefi:
-        from EfiPy2.MdePkg.Uefi.UefiMultiPhase import EfiResetPlatformSpecific
-        Reset.UefiOp (EfiResetPlatformSpecific)
+        print (f'Process "{UefiBootArg.help}"')
+        Reset.UefiOp (EfiResetPlatformSpecific, DryRun)
     else:
-        from EfiPy2.MdePkg.Uefi.UefiMultiPhase import EfiResetShutdown
-        Reset.UefiOp (EfiResetShutdown)
+        print (f'Process "{ShutdownArg.help}"')
+        Reset.UefiOp (EfiResetShutdown, DryRun)
 
 if __name__ == '__main__':
     main ()
